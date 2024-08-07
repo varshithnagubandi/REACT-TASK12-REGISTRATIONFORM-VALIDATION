22b19026add0d4135e714e437860479e28e6961e
vvvarshith <vvvarshith.nagubandi@gmail.com>
2024-08-07 10:28:27 +0530

    registrationform

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [data, setData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmpassword: "",
+  });
+
+  const inputHandle = (event) => {
+    setData({ ...data, [event.target.name]: event.target.value });
+  };
+
+  const submitHandle = (event) => {
+    event.preventDefault();
+    if (data.username.length <= 5) {
+      alert("USERNAME MUST GREATER THAN 5 CHARACTERS");
+      console.log("USERNAME MUST GREATER THAN 5 CHARACTERS");
+    } else if (data.password !== data.confirmpassword) {
+      alert("PASSWORD AND CONFIRM-PASSWORD NOT MATCHING");
+      console.log("PASSWORD AND CONFIRM-PASSWORD NOT MATCHING");
+    } else {
+      alert("THANK YOU FOR REGISTRATION");
+      console.log(data);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mainbox">
+      <center>
+        <h1 className="mainheading">REGISTRATION FORM</h1>
+        <form onSubmit={submitHandle}>
+          <label>USERNAME :</label>
+          <input
+            type="text"
+            name="username"
+            value={data.username}
+            onChange={inputHandle}
+          />
+          <br />
+          <label>E-MAIL :</label>
+          <input
+            type="email"
+            name="email"
+            value={data.email}
+            onChange={inputHandle}
+          />
+          <br />
+          <label>PASSWORD :</label>
+          <input
+            type="password"
+            name="password"
+            value={data.password}
+            onChange={inputHandle}
+          />
+          <br />
+          <label>CONFIRM PASSWORD :</label>
+          <input
+            type="password"
+            name="confirmpassword"
+            value={data.confirmpassword}
+            onChange={inputHandle}
+          />
+          <br />
+          <button type="submit">SUBMIT</button>
+        </form>
+        <h6 className="subheading">INSPECT TO SEE SUBMITTED DATA</h6>
+      </center>
     </div>
   );
 }
